e1bae3fe8f28c25f6aa283e8bd0d25446860aaf1
tirth_8205 <tirthkanani18@gmail.com>
2025-04-23 21:44:01 +0400

    Initial commit

--- utils/fetchSkills.ts
+++ utils/fetchSkills.ts
@@ -1,14 +1,12 @@
-
-
+import { groq } from "next-sanity";
+import { sanityClient } from "../sanity";
 import { Skill } from "../typings";
 
-export const fetchSkills = async() => {
-    const res = await fetch(`${process.env.NEXT_PUBLIC_BASE_URL}/api/getSkills`);
+const query = groq`
+    *[_type == "skill"]
+`;
 
-    const data = await res.json();
-    const skills: Skill[] = data.skills;
-
-    // console.log('fetching', skills);
-
+export const fetchSkills = async () => {
+    const skills: Skill[] = await sanityClient.fetch(query);
     return skills;
-}
+};
