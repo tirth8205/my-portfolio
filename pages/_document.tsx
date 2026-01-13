3600105ee3c4b3f7145bce442394161665cdebbe
Tirth Kanani <tirth@Tirths-MacBook-Pro-2.local>
2026-01-13 22:30:33 +0000

    Add dependencies and clean up project

--- pages/_document.tsx
+++ pages/_document.tsx
@@ -7,11 +7,9 @@ export default function Document() {
         <link rel="preconnect" href="https://fonts.googleapis.com" />
         <link rel="preconnect" href="https://fonts.gstatic.com" crossOrigin="anonymous" />
         <link
-          href="https://fonts.googleapis.com/css2?family=Inter:wght@400;500;600;700&display=swap"
+          href="https://fonts.googleapis.com/css2?family=Space+Grotesk:wght@400;500;600;700&display=swap"
           rel="stylesheet"
         />
-        <link rel="icon" href="/favicon.ico" />
-        <meta name="theme-color" content="#000000" />
       </Head>
       <body className="bg-background">
         <Main />
--- pages/index.tsx
+++ pages/index.tsx
@@ -62,16 +62,6 @@ export default function Home() {
         <meta name="twitter:description" content="AI Engineer with 3+ years building ML products. Expertise in Python, PyTorch, LLMs, MLOps, and Full-Stack development." />
         <meta name="twitter:creator" content="@tirthkanani" />
 
-        {/* Favicon */}
-        <link rel="icon" href="/favicon.ico" />
-
-        {/* Fonts */}
-        <link rel="preconnect" href="https://fonts.googleapis.com" />
-        <link rel="preconnect" href="https://fonts.gstatic.com" crossOrigin="anonymous" />
-        <link
-          href="https://fonts.googleapis.com/css2?family=Space+Grotesk:wght@400;500;600;700&display=swap"
-          rel="stylesheet"
-        />
       </Head>
 
       <div className="min-h-screen bg-black flex flex-col items-center justify-center px-6 relative overflow-hidden">
