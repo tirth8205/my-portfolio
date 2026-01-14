742c01a41c6098f2c17ac2660a09c0d672e686f1
Tirth Kanani <tirthkanani18@gmail.com>
2026-01-14 17:13:47 +0000

    updated sitemap settings

--- next-sitemap.config.js
+++ next-sitemap.config.js
@@ -1,5 +1,5 @@
 module.exports = {
-  siteUrl: 'https://www.tirthkanani.com',
+  siteUrl: 'https://tirthkanani.com',
   generateRobotsTxt: true, // Updates robots.txt with the sitemap location
   sitemapSize: 7000, // Maximum number of URLs per sitemap file (default is 50000)
   exclude: ['/wp-admin/*', '/wp-content/*', '/wp-includes/*', '/blog/*'], // Exclude WordPress paths
